e30eab30fb8ea925fe4bf1445111afe371a5cd64
YJP520 <76167345+YJP520@users.noreply.github.com>
2022-11-13 16:23:59 +0800

    commit_4

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -4,7 +4,7 @@
 // function.
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let vec0 = Vec::new();
@@ -20,7 +20,7 @@ fn main() {
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = Ver::new();
 
     vec.push(22);
     vec.push(44);
